d525cb9c38f00cdd23bca82a226d95ff2dcbc62b
Hirad <hharandian@gmail.com>
2018-12-26 00:09:17 +0100

    Init Commit

---
+++ Swoosh/Swoosh/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Harandian on 2018-12-26.
+//  Copyright © 2018 Hirad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Test")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
