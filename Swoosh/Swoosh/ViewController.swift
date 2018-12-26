bb13c9ee18bd9c3f9f8fdc7d05c5f79778fc66ab
Hirad <hharandian@gmail.com>
2018-12-26 03:30:33 +0100

    Finished 3 screen with segues

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    
+    @IBAction func unwindFromGenderVC (unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 }
 
